1b9322779ead3e7e5f74c24e2b0b0c303a13d2ff
feoche <francois.eoche@corp.ovh.com>
2020-03-04 11:01:52 +0100

    up

--- bot.js
+++ bot.js
@@ -31,11 +31,37 @@ function init() {
                     pollens: response,
                 }),
             );
+            city.pollens = response;
+        }).catch(() => {
+            cities.push(
+                Object.assign({}, city, {
+                    pollens: city.pollens || [],
+                }),
+            );
+        }).finally(() => {
             if (cities.length >= data.CITIES.length) {
+                // eslint-disable-next-line
+                console.info('cities:', cities);
+
+                const groupBy = (list, props) => {
+                    return list.reduce((a, b) => {
+                        (a[b[props]] = a[b[props]] || []).push(b);
+                        return a;
+                    }, {});
+                };
+
                 createTweetText(
-                    cities.sort((a, b) => {
-                        if (a.pollens.length !== b.pollens.length) { return b.pollens.length - a.pollens.length; } else { return b.label < a.label; }
-                    }).map((city) => [city.label, city.pollens.filter((pollen) => pollen !== "Graminée").map((pollen) => `${data.POLLENS[pollen]}${pollen}`).join(', ')]));
+                    Object.values(
+                        groupBy(
+                            cities
+                            .map((city) => ({label: city.label.replace("Saint", "St"), pollens: city.pollens.filter((pollen) => pollen !== 'Graminée')}))
+                            , 'pollens')
+                    )
+                    .map((cities) => ({cities: cities.map((city) => city.label), pollens: cities[0].pollens}))
+                    .sort((a, b) => {
+                        if (a.pollens.length !== b.pollens.length) { return b.pollens.length - a.pollens.length; } else { return b.cities.lenght > a.cities.length; }
+                    })
+                );
             }
         });
     });
@@ -44,11 +70,20 @@ function init() {
 init();
 
 function createTweetText(entries) {
-    let res = `Info pollens du ${format(new Date(), 'dd/MM/yy')} :\n`;
-    entries.map((entry) => {res += `${entry[0]} : ${entry[1]}\n`;});
+    let res = `${format(new Date(), 'dd/MM/yy')} :\n\n`;
+    entries.map((entry) => {res += `${entry.cities.join(', ')} : ${entry.pollens.map((pollen) => `${data.POLLENS[pollen]}${pollen}`).join(', ')}\n`;});
+
     // eslint-disable-next-line
     console.info('res:', res);
-    return res;
+
+    const tweets = res.match(/(.*?\n){1,8}/gmi);
+    // eslint-disable-next-line
+    console.info('tweets:', tweets);
+    tweet({ status:tweets[0]}).then((t) => {
+        if(tweets[1]) {
+            tweet({status: tweets[1], in_reply_to_status_id: t.id});
+        }
+    })
 }
 
 function tweet(options) {
@@ -73,48 +108,9 @@ function tweet(options) {
     }
 }
 
-function mergeArrays(arr1, arr2) {
-    let res = [];
-    for (let i = 0; i < arr1.length; i++) {
-        let found = false;
-        const a = arr1[i];
-        for (let j = 0; j < arr2.length; j++) {
-            const b = arr2[j];
-            if (a.slug === b.slug) {
-                found = true;
-                res.push({
-                    slug: a.slug,
-                    pictoTemps: a.pictoTemps,
-                    temperature: Math.max(a.temperature, b.temperature),
-                    temperatureMer: Math.max(a.temperatureMer, b.temperatureMer),
-                    pictoVent: a.pictoVent,
-                    forceVent: Math.max(a.forceVent, b.forceVent),
-                });
-            }
-        }
-        if (!found) {
-            res.push(a);
-        }
-    }
-    for (let i = 0; i < arr2.length; i++) {
-        let found = false;
-        let a = arr2[i];
-        for (let j = 0; j < arr1.length; j++) {
-            let b = arr1[j];
-            if (b.slug === a.slug) {
-                found = true;
-            }
-        }
-        if (!found) {
-            res.push(arr2[i]);
-        }
-    }
-    return res;
-}
-
 function queryWidget(url) {
     return new Promise((resolve, reject) => {
-        urllib.request(url, {timeout: 10000}, (error, data, response) => {
+        urllib.request(url, {timeout: 20000}, (error, data, response) => {
             if (error) {
                 console.error(
                     `\x1b[96m`, (`[` + new Date().toLocaleTimeString() + `]`).padStart(10),
@@ -127,69 +123,6 @@ function queryWidget(url) {
             }
         });
     });
-}
-
-function textToWeather(type) {
-    let index = 0, previousSpace = false;
-
-    const weather = index => dataItems.filter(a => a.slug === data.DATACITIES[index])[0];
-
-    return data.MAP.split('').map(char => {
-        switch (char) {
-            case '#':
-            case 'w':
-                let point = '',
-                    wcity = weather(index)[type];
-                switch (type) {
-                    case 'temperature' :
-                        if (previousSpace) {
-                            previousSpace = !previousSpace;
-                        } else {
-                            point += '\u2003';
-                        }
-                        point += ('' + wcity).replace(/\w/g, a => data.SMALL_LETTERS[a]);
-                        break;
-
-                    case 'temperatureMer' :
-                        if (previousSpace) {
-                            previousSpace = !previousSpace;
-                        }
-                        if (wcity > -1) {
-                            point += ('' + wcity).replace(/\w/g, a => data.SMALL_LETTERS[a]) +
-                                data.WEATHER.filter(a => (weather(index)['pictoTemps']).match(a.codes))[0].emojis[0];
-                        } else {
-                            point = char === 'w' ? '🌊' : '🏖';
-                        }
-                        break;
-                    case 'pictoVent' :
-                        const wforce = ('' + weather(index)['forceVent']).replace(/[\s<]/g, '').padStart(2, '0');
-                        const emoji = data.WINDS.filter(a => a.codes.includes(wcity))[0].emojis;
-                        point = emoji[Math.min(emoji.length - 1, Math.max(0, Math.floor(wforce[0]) - 1))];
-                        break;
-                    case 'pictoTemps' :
-                    default:
-                        const res = data.WEATHER.filter(a => wcity.match(a.codes))[0];
-                        if (!res) {
-                            console.info(
-                                `\x1b[96m`, (`[` + new Date().toLocaleTimeString() + `]`).padStart(10),
-                                'wcity : ', wcity);
-                        }
-                        point = res && res.emojis[0];
-                        break;
-                }
-                index++;
-                return point;
-            case '_':
-                previousSpace = true;
-                return '\u2003';
-            case '-':
-                previousSpace = true;
-                return '\u2002';
-            default:
-                previousSpace = true;
-                return char;
-        }
-    }).join('');
 }
 
 
